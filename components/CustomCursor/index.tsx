3694d50bab7ec434389b10269baf900627fd7b3e
literallymarvellous <ahiaraikechukwu@gmail.com>
2022-01-18 17:50:54 +0100

    added custom cursor

--- components/CustomCursor/index.tsx
+++ components/CustomCursor/index.tsx
@@ -1,12 +1,25 @@
-import { useEffect, useRef } from "react";
+import { MouseEvent, useEffect, useRef } from "react";
 import styles from "../../styles/components/Cursor.module.scss";
 
 const CustomCursor = () => {
-  const cursorRef = useRef(null);
+  const cursorRef = useRef<HTMLDivElement>(null!);
 
-  useEffect(() => {});
+  useEffect(() => {
+    console.log(cursorRef);
 
-  return <div className={styles.cursor}></div>;
+    document.addEventListener("mousemove", (e) => {
+      const { clientX, clientY } = e;
+      const mouseX = clientX - cursorRef.current.clientWidth / 2;
+      const mouseY = clientY - cursorRef.current.clientHeight / 2;
+      cursorRef.current.style.transform = `translate3d(
+        ${mouseX}px,
+        ${mouseY}px,
+        0
+      )`;
+    });
+  }, []);
+
+  return <div className={styles.cursor} ref={cursorRef}></div>;
 };
 
 export default CustomCursor;
