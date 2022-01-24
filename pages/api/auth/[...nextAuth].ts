ee694d55e78380378d5ecc47ac0045a38ccc5685
literallymarvellous <ahiaraikechukwu@gmail.com>
2022-01-24 16:26:24 +0100

    created scroll hook

--- pages/api/auth/[...nextAuth].ts
+++ pages/api/auth/[...nextAuth].ts
@@ -1,10 +1,8 @@
 import { NextApiHandler } from "next";
 import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import EmailProvider from "next-auth/providers/email";
 import prisma from "../../../lib/prisma";
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
